8a70995affc7283ac128485e82670cc1ed673fdf
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2017-12-04 19:50:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,6 @@ function specialAlert(a) {
     var myWindow = window.open("", "", "width=200,height=100");
     myWindow.document.write("<script src='https://rawgit.com/amazinigmech2418/SuperSite/master/init.js'></script><button onclick='window.close();'>Close</button><br/>"+a);
 }
-var htmlData = "
-<button onclick='bringItBack(); window.close();'>#BringItBack</button><br/>
-<button onclick='consoleType(); window.close();'>JS Console</button><br/>
-<button onclick='gmailMoji(); window.close();'>gmail -> emojis</button><br/>
-
-";
+var htmlData = "<button onclick='bringItBack(); window.close();'>#BringItBack</button><br/><button onclick='consoleType(); window.close();'>JS Console</button><br/><button onclick='gmailMoji(); window.close();'>gmail -> emojis</button><br/>";
 
 specialAlert(htmlData);
