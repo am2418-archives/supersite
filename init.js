30b77e21c37cb8a95657f7f954ee5c461e6e81f3
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2017-12-04 20:00:56 -0500

    Update init.js

--- init.js
+++ init.js
@@ -1,20 +1,20 @@
 function consoleType() {
-var consoleLatest = myWindow.opener.eval(prompt("Input your code here."));
-if(myWindow.opener.confirm("Do you want a return function of your code?")) {
-myWindow.opener.alert(consoleLatest);
+var consoleLatest = window.opener.eval(prompt("Input your code here."));
+if(window.opener.confirm("Do you want a return function of your code?")) {
+window.opener.alert(consoleLatest);
 }
 }
 
 function bringItBack() {
-if(myWindow.opener.location.hostname === "scratch.mit.edu") {
-myWindow.opener.document.getElementsByTagName("a")[3].innerHTML = "Discuss";
-myWindow.opener.document.getElementsByTagName("a")[3].href = "/discuss";
+if(window.opener.location.hostname === "scratch.mit.edu") {
+window.opener.document.getElementsByTagName("a")[3].innerHTML = "Discuss";
+window.opener.document.getElementsByTagName("a")[3].href = "/discuss";
 }
 }
 
 function gmailMoji() {
-if(myWindow.opener.location.hostname === "mail.google.com") {
-var divs = myWindow.opener.document.getElementsByTagName("div");
+if(window.opener.location.hostname === "mail.google.com") {
+var divs = window.opener.document.getElementsByTagName("div");
 for(i=0;i<divs.length;i++){
 divs[i].innerHTML = "10110101001010";
 }
