8b40a3195f46aa7706829c1a662cf6fc0a29b409
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2017-12-06 16:54:45 -0500

    Update customexample.js

--- customexample.js
+++ customexample.js
@@ -1,7 +1,5 @@
-function clear() {
-window.opener.write("");
-}
+
 //Change amazinigmech2418 to your username. Change supersite to the repository. Change customexample.js to the file name.
-customsURLs.push("https://rawgit.com/amazinigmech2418/SuperSite/master/customexample.js");
+customsURLs.push("https://rawgit.com/amazinigmech2418/SuperSite/master/customexampledefinition.js");
 //This is an example of HTML code for the window.
 customHTMLs += "<button onclick='clear();window.close();'>Clear Screen</button>";
