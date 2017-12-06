5e51dff73e81ca1cee3ce44c9b93ea7e41222781
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2017-12-06 13:22:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var myWindow;
 function specialAlert(a) {
     var myWindow = window.open("", "", "width=200,height=100");
-    myWindow.document.write("<script src='https://rawgit.com/amazinigmech2418/SuperSite/master/init.js'></script><button onclick='window.close();'>Close</button><br/>"+a);
+    myWindow.document.write("<script src='https://rawgit.com/amazinigmech2418/SuperSite/master/init.js'></script><script src='https://rawgit.com/amazinigmech2418/mobilescratch/master/supersitemain.js'></script><button onclick='window.close();'>Close</button><br/>"+a);
 }
-var htmlData = "<button onclick='bringItBack(); window.close();'>#BringItBack</button><br/><button onclick='consoleType(); window.close();'>JS Console</button><br/><button onclick='gmailMoji(); window.close();'>gmail -> emojis</button><br/>";
+var htmlData = "<button onclick='bringItBack(); window.close();'>#BringItBack</button><br/><button onclick='consoleType(); window.close();'>JS Console</button><br/><button onclick='gmailMoji(); window.close();'>gmail -> emojis [broken]</button><br/><button onclick='phosphorus(); window.close();'>mobileScratch</button><br/>";
 
 specialAlert(htmlData);
